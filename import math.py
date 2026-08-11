e5065b6d2891b6ea0e667b7e560413ad2a0420bd
pldarboleda31-boop <pldarboleda31@src.pshs.edu.ph>
2026-08-11 09:48:59 +0800

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,7 @@
+import math
+x1 =input("Enter x1:")
+y1 =input("Enter y1: ")
+x2 =input("Enter x2: ")
+y2 =input("Enter y2: ")
+distance = math.sqrt(math.pow((x2 - x1),2) + math.pow((y2 -y1), 2))
+print (distance)
